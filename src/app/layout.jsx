ac0dc3d36ae5b4bd95d97ed150433a7432ce3919
Shifat Ali <shifatali0906@gmail.com>
2026-02-03 12:03:00 +0530

    layout

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -6,6 +6,9 @@ import ScrollToTop from "@/components/layout/ScrollToTop";
 export const metadata = {
   title: "Infinity Space Group",
   description: "Infinity Space Group - Architecture and Design Services",
+  verification: {
+    google: "TFm2HPD6vlLmT6oYgm63aHU3ir1irNKViEPqQAjlBAc",
+  },
 };
 
 export default function RootLayout({ children }) {
