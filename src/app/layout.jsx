0f5d1cf95ad29067bcc349d1f101f0d5d68827fd
Shifat Ali <shifatali0906@gmail.com>
2026-02-03 12:39:20 +0530

    layout changes

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -6,8 +6,15 @@ import ScrollToTop from "@/components/layout/ScrollToTop";
 export const metadata = {
   title: "Infinity Space Group",
   description: "Infinity Space Group - Architecture and Design Services",
+
+  // Google Search Console verification
   verification: {
     google: "TFm2HPD6vlLmT6oYgm63aHU3ir1irNKViEPqQAjlBAc",
+  },
+
+  // Facebook Domain Verification
+  other: {
+    "facebook-domain-verification": "q7wwvrbp1g0bwg4jfl5jnr9agwu5ax",
   },
 };
 
